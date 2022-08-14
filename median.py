13fe9c4be461653212558d63a55296eaf7eb259b
Deekshit-CICS <61113013+Deekshit-CICS@users.noreply.github.com>
2022-08-14 18:46:37 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,7 @@
+# Import statistics Library
+import statistics 
+
+# Calculate middle values
+print(statistics.median([1, 3, 5, 7, 9, 11, 13]))
+print(statistics.median([1, 3, 5, 7, 9, 11]))
+print(statistics.median([-11, 5.5, -3.4, 7.1, -9, 22]))
